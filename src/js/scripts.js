d0c3799131d56d0c88e97bc7cd02d80aab93661a
Stu Cowley <stucowley@outlook.com>
2025-07-14 21:14:58 +0930

    chore(sceripts.js): initial commit

---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+/**
+ * SITE SCRIPTS
+ *
+ * scripts.js contains all JavaScript functionality for the site, including UI
+ * interactions, component behaviour, and event handling. Scripts are written
+ * with performance and minimalism in mind, avoiding unnecessary bloat while
+ * enhancing usability and accessibility.
+ */
+
+// Test if scripts.js has loaded
+console.log("Hello from scripts.js");
