b1bc0093ef0cc312246da818e62636d16a5c4439
Stu Cowley <stucowley@outlook.com>
2025-07-18 15:17:40 +0930

    feat(scripts.js): add header menu toggle script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,4 +8,53 @@
  */
 
 // Test if scripts.js has loaded
-console.log("Hello from scripts.js");
+console.log('Hello from scripts.js');
+
+/**
+ * HEADER MENU TOGGLE
+ * -----------------------------------------------------------------------------
+ *
+ * Adds interactive behaviour to the site header's mobile navigation toggle
+ * button.
+ *
+ * - Toggles the visibility of the header menu by adding/removing the
+ *   `data-visible` attribute
+ * - Updates the `aria-expanded` attribute to ensure accessibility for screen
+ *   readers.
+ * - Adds a click listener to the document to close the menu when tapping
+ *   outside of the header menu.
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+    const headerMenuToggle = document.querySelector('.header-menu-toggle');
+    const headerMenu = document.querySelector('#header-menu');
+
+    headerMenuToggle.addEventListener('click', function () {
+        const isMenuVisible = headerMenu.hasAttribute('data-visible');
+
+        if (isMenuVisible) {
+            headerMenu.removeAttribute('data-visible');
+            headerMenuToggle.setAttribute('aria-expanded', 'false');
+        } else {
+            headerMenu.setAttribute('data-visible', '');
+            headerMenuToggle.setAttribute('aria-expanded', 'true');
+
+            document.addEventListener('click', closeMenuOnTapOutside);
+        }
+    });
+
+    /**
+     * Closes the header menu when a user taps outside the toggle button or the
+     * header menu
+     */
+    function closeMenuOnTapOutside(event) {
+        if (
+            !headerMenu.contains(event.target) &&
+            !headerMenuToggle.contains(event.target)
+        ) {
+            headerMenu.removeAttribute('data-visible');
+            headerMenuToggle.setAttribute('aria-expanded', 'false');
+            document.removeEventListener('click', closeMenuOnTapOutside);
+        }
+    }
+});
